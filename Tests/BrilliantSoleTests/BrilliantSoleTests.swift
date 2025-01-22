1b8655a538995fd8250ebbe02386c9abcc1fce3c
Zack <zack@ukaton.com>
2025-01-21 17:33:04 -0800

    Update BrilliantSoleTests.swift

--- Tests/BrilliantSoleTests/BrilliantSoleTests.swift
+++ Tests/BrilliantSoleTests/BrilliantSoleTests.swift
@@ -1,46 +1,48 @@
 @testable import BrilliantSole
 import Testing
 
-struct BSSensorConfigurationTests {
-    @Test func parseConfigurationTest() async throws {
-        let configuration: BSSensorConfiguration = [.acceleration: ._80ms, .gameRotation: ._20ms]
-        print("sensorConfiguration: \(configuration)")
-        let configurationData = configuration.getData()
-        print("sensorConfigurationData: \(configurationData.bytes)")
-        let parsedConfiguration = BSSensorConfiguration.parse(configurationData)
-        #expect(parsedConfiguration != nil)
-        print("parsedConfiguration: \(String(describing: parsedConfiguration!))")
-        #expect(configuration == parsedConfiguration)
-    }
-}
-
-struct BSTfliteTests {
-    @Test func loadTfliteFileTest() async throws {
-        var tfliteFile: BSTfliteFile = .init(
-            fileName: "model",
-            modelName: "gestures",
-            sensorTypes: [.linearAcceleration, .gyroscope],
-            task: .classification,
-            sensorRate: ._10ms,
-            classes: ["idle", "kick", "stomp", "tap"]
-        )
-        let data = tfliteFile.getFileData(bundle: .module)
-        #expect(data != nil)
-        print("checksum: \(data!.crc32())")
-        #expect(data!.crc32() == 200001559)
-    }
-}
-
-struct BSVibrationTests {
-    @Test func vibrationWaveformTest() async throws {
-        let configuration: BSVibrationConfiguration = .init(locations: [.front, .rear], waveformSegments: [.init(amplitude: 0.5, duration: 100)])
-        let configurationData = configuration.getData()
-        print("vibrationWaveform data: \(configurationData.bytes)")
+enum BSTests {
+    struct BSSensorConfigurationTests {
+        @Test func parseConfigurationTest() async throws {
+            let configuration: BSSensorConfiguration = [.acceleration: ._80ms, .gameRotation: ._20ms]
+            print("sensorConfiguration: \(configuration)")
+            let configurationData = configuration.getData()
+            print("sensorConfigurationData: \(configurationData.bytes)")
+            let parsedConfiguration = BSSensorConfiguration.parse(configurationData)
+            #expect(parsedConfiguration != nil)
+            print("parsedConfiguration: \(String(describing: parsedConfiguration!))")
+            #expect(configuration == parsedConfiguration)
+        }
     }
 
-    @Test func vibrationWaveformEffectTest() async throws {
-        let configuration: BSVibrationConfiguration = .init(locations: [.front, .rear], waveformEffectSegments: [.init(effect: .alert1000ms, loopCount: 0)], loopCount: 0)
-        let configurationData = configuration.getData()
-        print("vibrationWaveformEffects data: \(configurationData.bytes)")
+    struct BSTfliteTests {
+        @Test func loadTfliteFileTest() async throws {
+            var tfliteFile: BSTfliteFile = .init(
+                fileName: "model",
+                modelName: "gestures",
+                sensorTypes: [.linearAcceleration, .gyroscope],
+                task: .classification,
+                sensorRate: ._10ms,
+                classes: ["idle", "kick", "stomp", "tap"]
+            )
+            let data = tfliteFile.getFileData(bundle: .module)
+            #expect(data != nil)
+            print("checksum: \(data!.crc32())")
+            #expect(data!.crc32() == 200001559)
+        }
+    }
+
+    struct BSVibrationTests {
+        @Test func vibrationWaveformTest() async throws {
+            let configuration: BSVibrationConfiguration = .init(locations: [.front, .rear], waveformSegments: [.init(amplitude: 0.5, duration: 100)])
+            let configurationData = configuration.getData()
+            print("vibrationWaveform data: \(configurationData.bytes)")
+        }
+
+        @Test func vibrationWaveformEffectTest() async throws {
+            let configuration: BSVibrationConfiguration = .init(locations: [.front, .rear], waveformEffectSegments: [.init(effect: .alert1000ms, loopCount: 0)], loopCount: 0)
+            let configurationData = configuration.getData()
+            print("vibrationWaveformEffects data: \(configurationData.bytes)")
+        }
     }
 }
