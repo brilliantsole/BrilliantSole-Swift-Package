a73cb4b6676b05f0f6d9bec21caf2ef27b83aa9c
Zack <zack@ukaton.com>
2025-01-22 23:30:05 -0800

    /

--- Sources/BrilliantSole/Device/Battery/BSBatteryManager.swift
+++ Sources/BrilliantSole/Device/Battery/BSBatteryManager.swift
@@ -5,6 +5,7 @@
 //  Created by Zack Qattan on 1/22/25.
 //
 
+import Combine
 import OSLog
 import UkatonMacros
 
@@ -17,9 +18,37 @@ class BSBatteryManager: BSBaseManager<BSBatteryMessageType> {
     override func onRxMessage(_ messageType: BSBatteryMessageType, data: Data) {
         switch messageType {
         case .getIsBatteryCharging:
-            print("FILL")
+            parseIsBatteryCharging(data)
         case .getBatteryCurrent:
-            print("FILL")
+            parseBatteryCurrent(data)
         }
+    }
+
+    override func reset() {
+        super.reset()
+        isBatteryChargingSubject.value = false
+        batteryCurrentSubject.value = 0
+    }
+
+    // MARK: - isBatteryCharging
+
+    var isBatteryChargingSubject = CurrentValueSubject<Bool, Never>(false)
+    var isBatteryCharging: Bool { isBatteryChargingSubject.value }
+
+    func parseIsBatteryCharging(_ data: Data) {
+        let newIsBatteryCharging: Bool = data[0] == 1
+        logger.debug("parsed isBatteryCharging: \(newIsBatteryCharging)")
+        isBatteryChargingSubject.value = newIsBatteryCharging
+    }
+
+    // MARK: - batteryCurrent
+
+    var batteryCurrentSubject = CurrentValueSubject<Float, Never>(0)
+    var batteryCurrent: Float { batteryCurrentSubject.value }
+
+    func parseBatteryCurrent(_ data: Data) {
+        let newBatteryCurrent: Float = .parse(data, at: 0)
+        logger.debug("parsed batteryCurrent: \(newBatteryCurrent)")
+        batteryCurrentSubject.value = newBatteryCurrent
     }
 }
